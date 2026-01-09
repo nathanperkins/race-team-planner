ebfd7374208b567e9bf4764e0953a36edb665f5d
Nathan Perkins <nperkins487@gmail.com>
2026-01-09 02:15:39 -0800

    chore: add cleanup to registry

--- terraform/registry.tf
+++ terraform/registry.tf
@@ -4,5 +4,23 @@ resource "google_artifact_registry_repository" "app_repo" {
   description   = "Docker repository for ${var.app_name}"
   format        = "DOCKER"
 
+  cleanup_policy_dry_run = false
+
+  cleanup_policies {
+    id     = "keep-recent-versions"
+    action = "KEEP"
+    most_recent_versions {
+      keep_count = 3
+    }
+  }
+
+  cleanup_policies {
+    id     = "delete-old-versions"
+    action = "DELETE"
+    condition {
+      older_than = "604800s" # 7 days
+    }
+  }
+
   depends_on = [google_project_service.apis]
 }
