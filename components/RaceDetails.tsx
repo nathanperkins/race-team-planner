b71743ce4c938b4755b5351a93d0edf1af231cab
Nathan Perkins <nperkins487@gmail.com>
2026-02-09 18:17:06 -0800

    style: update discussion links with heroicons and improved hover effects

--- components/RaceDetails.tsx
+++ components/RaceDetails.tsx
@@ -188,7 +188,7 @@ function MaxDriversPerTeamInput({
   )
 }
 
-const DiscordIcon = ({ size = 16, className }: { size?: number; className?: string }) => (
+const DiscussionIcon = ({ size = 16, className }: { size?: number; className?: string }) => (
   <svg
     width={size}
     height={size}
@@ -197,7 +197,8 @@ const DiscordIcon = ({ size = 16, className }: { size?: number; className?: stri
     className={className}
     xmlns="http://www.w3.org/2000/svg"
   >
-    <path d="M20.317 4.37a19.791 19.791 0 0 0-4.885-1.515.074.074 0 0 0-.079.037c-.21.375-.444.864-.608 1.25a18.27 18.27 0 0 0-5.487 0 12.64 12.64 0 0 0-.617-1.25.077.077 0 0 0-.079-.037A19.736 19.736 0 0 0 3.677 4.37a.07.07 0 0 0-.032.027C.533 9.046-.32 13.58.099 18.057a.082.082 0 0 0 .031.057 19.9 19.9 0 0 0 5.993 3.03.078.078 0 0 0 .084-.028 14.09 14.09 0 0 0 1.226-1.994.076.076 0 0 0-.041-.106 13.107 13.107 0 0 1-1.872-.892.077.077 0 0 1-.008-.128 10.2 10.2 0 0 0 .372-.292.074.074 0 0 1 .077-.01c3.928 1.793 8.18 1.793 12.062 0a.074.074 0 0 1 .078.01c.12.098.246.198.373.292a.077.077 0 0 1-.006.127 12.299 12.299 0 0 1-1.873.892.077.077 0 0 0-.041.107c.36.698.772 1.362 1.225 1.993a.076.076 0 0 0 .084.028 19.839 19.839 0 0 0 6.002-3.03.077.077 0 0 0 .032-.054c.5-5.177-.838-9.674-3.549-13.66a.061.061 0 0 0-.031-.03zM8.02 15.33c-1.183 0-2.157-1.085-2.157-2.419 0-1.333.955-2.419 2.157-2.419 1.21 0 2.176 1.096 2.157 2.42 0 1.333-.956 2.418-2.157 2.418zm7.975 0c-1.183 0-2.157-1.085-2.157-2.419 0-1.333.955-2.419 2.157-2.419 1.21 0 2.176 1.096 2.157 2.42 0 1.333-.946 2.418-2.157 2.418z" />
+    <path d="M4.913 2.658c2.075-.21 4.19-.322 6.337-.322 2.146 0 4.262.112 6.337.322a.75.75 0 0 1 .677.743V12a.75.75 0 0 1-.677.743 48.33 48.33 0 0 1-3.411.308c-1.856.118-3.598.804-5.048 1.943l-2.408 1.9a.75.75 0 0 1-1.223-.585V14.54a.75.75 0 0 0-.75-.75 49.784 49.784 0 0 1-3.033-.376.75.75 0 0 1-.627-.74V3.401a.75.75 0 0 1 .676-.743Z" />
+    <path d="M15.857 15.346c.866.044 1.72.11 2.565.196l2.181 1.735c.539.429 1.341.047 1.341-.64V12.94a.75.75 0 0 1 .75-.75 48.138 48.138 0 0 1 1.05.037.75.75 0 0 1 .69.742V18.4a.75.75 0 0 1-.674.745 49.14 49.14 0 0 1-5.18.398c-1.58.079-3.054.67-4.26 1.63L11.7 22.18c-.538.43-1.34.047-1.34-.64V19.34a.75.75 0 0 1 .75-.75 48.307 48.307 0 0 1 1.62-.123c1.245-.142 2.391-.643 3.127-1.511Z" />
   </svg>
 )
 
@@ -1806,7 +1807,7 @@ export default function RaceDetails({
                             title="Join the team discussion on Discord"
                             onClick={(e) => e.stopPropagation()}
                           >
-                            <DiscordIcon size={16} />
+                            <DiscussionIcon size={16} />
                           </a>
                         )
                       }
@@ -1815,7 +1816,7 @@ export default function RaceDetails({
                           className={`${styles.discordLink} ${styles.discordLinkInactive}`}
                           title="The team thread has not been generated yet"
                         >
-                          <DiscordIcon size={16} />
+                          <DiscussionIcon size={16} />
                         </span>
                       )
                     })()}
@@ -2084,7 +2085,7 @@ export default function RaceDetails({
                   className={styles.discordLink}
                   title="Join the event discussion in Discord"
                 >
-                  <DiscordIcon size={20} />
+                  <DiscussionIcon size={20} />
                 </a>
               )
             }
@@ -2094,7 +2095,7 @@ export default function RaceDetails({
                 style={{ marginLeft: '12px' }}
                 title="The event thread has not been generated yet"
               >
-                <DiscordIcon size={20} />
+                <DiscussionIcon size={20} />
               </span>
             )
           })()}
